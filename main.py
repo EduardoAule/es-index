f6b5575a92345ac28f1bfaee021e407508e84af1
Eduardo R. <erico@hebmex.com>
2025-08-05 12:31:46 -0600

    Test upload script

--- main.py
+++ main.py
@@ -3,12 +3,14 @@ from elasticsearch import Elasticsearch
 from datetime import datetime, timedelta
 from pytz import timezone, UTC
 import json
+from dotenv import load_dotenv
+
+load_dotenv()
 
 tz = timezone('America/Mexico_City')
 # agentes
-AGENTS = json.loads(os.environ['AGENTS_COMME'])
-agents = ["ims-worker", "ims-rest"]
-OLDERS_WITH_DAYS = 20
+AGENTS = json.loads(os.environ['AGENTS_NAMES'])
+OLDERS_WITH_DAYS = os.getenv('OLDERS_WITH_DAYS')
 
 es = Elasticsearch("http://192.167.36.108:9200")
 
@@ -18,7 +20,7 @@ es = Elasticsearch("http://192.167.36.108:9200")
 ds = datetime.now(tz)
 # print("ds:", ds)
 # datestring = ds.strftime(formatf)
-dt_last_days = ds - timedelta(days=OLDERS_WITH_DAYS)
+dt_last_days = ds - timedelta(days = int(OLDERS_WITH_DAYS))
 
 print("datestring:", dt_last_days, ds)
 
@@ -31,7 +33,7 @@ indices = es.cat.indices( format="json")
 # Filtrar por nombre de agente
 for index in indices:
     index_name = index['index']
-    for a in agents:
+    for a in AGENTS:
         # indice contiene el nombre de un agente valido
         if index_name.__contains__(a):
             print(f"Yes! {index_name} is containing.")
@@ -43,13 +45,18 @@ for index in indices:
 # print(agents_indices)
 # Recorrer los indices con el patron
 # Obtener su fecha de creacion y eliminar
-for aindex in agents_indices:
-    # print(aindex['settings']['index']['provided_name'])
-    index_name = aindex['settings']['index']['provided_name']
-    my_datetime = datetime.fromtimestamp(int(aindex['settings']['index']['creation_date']) / 1000)
-    index_datetime = my_datetime.replace(tzinfo=UTC)
-    if index_datetime > dt_last_days:
-        print(index_name, my_datetime, "vigente")
-    else:
-        print(index_name, my_datetime, "old")
-        # es.indices.delete(index=index_name)
+try:
+    for aindex in agents_indices:
+        # print(aindex['settings']['index']['provided_name'])
+        index_name = aindex['settings']['index']['provided_name']
+        my_datetime = datetime.fromtimestamp(int(aindex['settings']['index']['creation_date']) / 1000)
+        index_datetime = my_datetime.replace(tzinfo=UTC)
+        if index_datetime > dt_last_days:
+            print(index_name, index_datetime, "vigente")
+        else:
+            # print(index_name, my_datetime, "old")
+            # es.indices.delete(index=index_name)
+            print("Delete index %s with creation_date %s" % (index_name, index_datetime))
+
+except Exception as e:
+    print("Cannot delete index: %s exc: %s" % (index_name, e))
